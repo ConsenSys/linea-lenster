377ec628446f118935e99ad351c7ac489ffe27e1
Alain Nicolas <alainncls@users.noreply.github.com>
2023-04-25 07:58:51 +0200

    LENS-41 - Toggle dispatcher mode on account creation (#12)

--- apps/web/src/components/Shared/Login/New/index.tsx
+++ apps/web/src/components/Shared/Login/New/index.tsx
@@ -1,6 +1,8 @@
+import ChooseFile from '@components/Shared/ChooseFile';
 import useSimpleDebounce from '@components/utils/hooks/useSimpleDebounce';
 import { PlusIcon } from '@heroicons/react/outline';
 import { Regex } from '@lenster/data';
+import uploadToIPFS from '@lib/uploadToIPFS';
 import {
   Button,
   ErrorMessage,
@@ -12,13 +14,15 @@ import {
 import { t, Trans } from '@lingui/macro';
 import {
   APP_NAME,
+  IS_RELAYER_AVAILABLE,
   LENS_PROFILE_CREATOR,
   LENS_PROFILE_CREATOR_ABI,
   ZERO_ADDRESS
 } from 'data/constants';
+import { useCreateProfileMutation } from 'lens';
 import getStampFyiURL from 'lib/getStampFyiURL';
-import type { FC } from 'react';
-import React, { useState } from 'react';
+import type { ChangeEvent, FC } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   useAccount,
   useContractWrite,
@@ -26,6 +30,8 @@ import {
   useWaitForTransaction
 } from 'wagmi';
 import { object, string } from 'zod';
+
+import Pending from './Pending';
 
 const newUserSchema = object({
   handle: string()
@@ -45,7 +51,9 @@ const NewProfile: FC<NewProfileProps> = ({ isModal = false }) => {
 
   const [handle, setHandle] = useState('');
   const [avatar, setAvatar] = useState(getStampFyiURL(address ?? ZERO_ADDRESS));
-  //const [uploading, setUploading] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [createProfile, { data, loading }] = useCreateProfileMutation();
+  const [isCreationLoading, setIsCreationLoading] = useState(false);
 
   const debouncedHandle = useSimpleDebounce(handle);
 
@@ -62,17 +70,17 @@ const NewProfile: FC<NewProfileProps> = ({ isModal = false }) => {
         to: address,
         handle: debouncedHandle,
         imageURI: avatar,
-        followModule: '0x0000000000000000000000000000000000000000',
+        followModule: ZERO_ADDRESS,
         followModuleInitData: '0x',
         followNFTURI: ''
       }
     ],
     enabled: Boolean(debouncedHandle)
   });
-  const { data, write } = useContractWrite(config);
+  const { data: txData, write } = useContractWrite(config);
 
   const { isLoading, error } = useWaitForTransaction({
-    hash: data?.hash
+    hash: txData?.hash
   });
 
   /*const handleUpload = async (event: ChangeEvent<HTMLInputElement>) => {
@@ -99,15 +107,48 @@ const NewProfile: FC<NewProfileProps> = ({ isModal = false }) => {
     write?.();
   };
 
-  return (
-    <Form form={form} className="space-y-4" onSubmit={handleCreateProfile}>
-      {error && (
+  useEffect(() => {
+    setIsCreationLoading(
+      IS_RELAYER_AVAILABLE ? data?.createProfile.__typename === 'RelayerResult' : !!txData?.hash
+    );
+  }, [data, txData]);
+
+  return isCreationLoading ? (
+    <Pending
+      handle={form.getValues('handle')}
+      txHash={
+        IS_RELAYER_AVAILABLE && data?.createProfile.__typename === 'RelayerResult'
+          ? data?.createProfile?.txHash
+          : txData?.hash
+      }
+    />
+  ) : (
+    <Form
+      form={form}
+      className="space-y-4"
+      onSubmit={({ handle }) => {
+        if (IS_RELAYER_AVAILABLE) {
+          const username = handle.toLowerCase();
+          createProfile({
+            variables: {
+              request: {
+                handle: username,
+                profilePictureUri: avatar || getStampFyiURL(address ?? ZERO_ADDRESS)
+              }
+            }
+          });
+        } else {
+          handleCreateProfile();
+        }
+      }}
+    >
+      {data?.createProfile.__typename === 'RelayError' && (data?.createProfile.reason || error) && (
         <ErrorMessage
           className="mb-3"
           title="Create profile failed!"
           error={{
             name: 'Create profile failed!',
-            message: error.message
+            message: IS_RELAYER_AVAILABLE ? data?.createProfile?.reason : error?.message || ''
           }}
         />
       )}
@@ -147,7 +188,6 @@ const NewProfile: FC<NewProfileProps> = ({ isModal = false }) => {
               />
             </div>
           )}
-          {/* TODO: reactivate avatar upload once we have a S3 bucket
           <div>
             <div className="flex items-center space-x-3">
               <ChooseFile
@@ -157,13 +197,13 @@ const NewProfile: FC<NewProfileProps> = ({ isModal = false }) => {
               />
               {uploading && <Spinner size="sm" />}
             </div>
-          </div>*/}
+          </div>
         </div>
       </div>
       <Button
         className="ml-auto"
         type="submit"
-        disabled={!write || isLoading || !isConnected}
+        disabled={!write || loading || isLoading || !isConnected}
         icon={
           isLoading ? <Spinner size="xs" /> : <PlusIcon className="h-4 w-4" />
         }
