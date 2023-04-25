577e7981abbdf0f6a97716ce0485e07444ff80fd
Alain Nicolas <alainncls@users.noreply.github.com>
2023-04-25 07:59:13 +0200

    LENS-34 - Hide 'Dispatcher' and 'Allowance' tabs from the Settings page (#14)

--- apps/web/src/components/Settings/Sidebar.tsx
+++ apps/web/src/components/Settings/Sidebar.tsx
@@ -12,6 +12,7 @@ import {
 } from '@heroicons/react/outline';
 import type { Profile } from '@lenster/lens';
 import { t, Trans } from '@lingui/macro';
+import { IS_RELAYER_AVAILABLE } from 'data';
 import type { FC } from 'react';
 import { useAppStore } from 'src/store/app';
 
@@ -43,16 +44,24 @@ const SettingsSidebar: FC = () => {
             icon: <BookmarkIcon className="h-4 w-4" />,
             url: '/settings/interests'
           },
-          {
-            title: t`Dispatcher`,
-            icon: <FingerPrintIcon className="h-4 w-4" />,
-            url: '/settings/dispatcher'
-          },
-          {
-            title: t`Allowance`,
-            icon: <ShareIcon className="h-4 w-4" />,
-            url: '/settings/allowance'
-          },
+          ...(IS_RELAYER_AVAILABLE
+            ? [
+                {
+                  title: t`Dispatcher`,
+                  icon: <FingerPrintIcon className="h-4 w-4" />,
+                  url: '/settings/dispatcher'
+                }
+              ]
+            : []),
+          ...(IS_RELAYER_AVAILABLE
+            ? [
+                {
+                  title: t`Allowance`,
+                  icon: <ShareIcon className="h-4 w-4" />,
+                  url: '/settings/allowance'
+                }
+              ]
+            : []),
           {
             title: t`Cleanup`,
             icon: <SparklesIcon className="h-4 w-4" />,
