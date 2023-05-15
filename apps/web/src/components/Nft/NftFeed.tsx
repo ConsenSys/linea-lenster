895bd0ad57b005cbb70386ae6406c2a331b2f384
Alain Nicolas <alain.nicolas@consensys.net>
2023-05-15 16:00:15 +0200

    Fixes after review

--- apps/web/src/components/Nft/NftFeed.tsx
+++ apps/web/src/components/Nft/NftFeed.tsx
@@ -1,13 +1,13 @@
 import SingleNft from '@components/Nft/SingleNft';
 import { CollectionIcon } from '@heroicons/react/outline';
-import { Trans } from '@lingui/macro';
+import { t, Trans } from '@lingui/macro';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import type { Profile } from 'lens';
 import formatHandle from 'lib/formatHandle';
 import type { FC } from 'react';
-import { useEffect, useState } from 'react';
-import { EmptyState } from 'ui';
+import { useMemo } from 'react';
+import { EmptyState, ErrorMessage } from 'ui';
 
 import type { NftLinea, RawNfts } from '../../types';
 
@@ -16,8 +16,6 @@ interface NftFeedProps {
 }
 
 const NftFeed: FC<NftFeedProps> = ({ profile }) => {
-  const [nfts, setNfts] = useState<NftLinea[]>([]);
-
   const getNfts = async () => {
     const response = await axios<RawNfts>({
       url: 'https://api-testnet.nftnest.io/v1/wallet/get_nfts',
@@ -35,39 +33,44 @@ const NftFeed: FC<NftFeedProps> = ({ profile }) => {
     return response.data;
   };
 
-  const { data: rawNfts, isLoading: isGalleryLoading } = useQuery([], () => getNfts().then((res) => res));
+  const {
+    data: rawNfts,
+    error: nftsError,
+    isLoading: isGalleryLoading
+  } = useQuery([], () => getNfts().then((res) => res));
 
-  useEffect(() => {
+  const collections = useMemo(() => {
     if (rawNfts) {
-      const collections = Object.entries(rawNfts.contracts['59140']).map((entry) => {
+      return Object.entries(rawNfts.contracts['59140']).map((entry) => {
         return {
           contract_address: entry[0],
           collection_info: entry[1].collection_info
         };
       });
-
-      const ownedNfts: NftLinea[] = rawNfts.tokens['59140']?.map((nft) => {
-        const collection = collections.find((collection) => {
-          return collection.contract_address === nft?.contract_address;
-        });
-
-        return {
-          contractAddress: collection?.contract_address,
-          collectionName: collection?.collection_info?.name,
-          contractName: collection?.collection_info?.name,
-          chainId: 59140,
-          tokenId: parseInt(nft.token_id, 16),
-          name: nft.token_info?.metadata?.name,
-          description: nft.token_info?.metadata?.description,
-          originalContent: { uri: nft.token_info?.metadata?.image }
-        };
-      });
-
-      setNfts(ownedNfts);
     }
   }, [rawNfts]);
 
-  if (nfts?.length === 0) {
+  const ownedNfts: NftLinea[] = useMemo(() => {
+    return rawNfts
+      ? rawNfts.tokens['59140']?.map((nft) => {
+          const collection = collections?.find((collection) => {
+            return collection.contract_address === nft?.contract_address;
+          });
+
+          return {
+            contractAddress: collection?.contract_address,
+            collectionName: collection?.collection_info?.name,
+            contractName: collection?.collection_info?.name,
+            chainId: 59140,
+            tokenId: parseInt(nft.token_id, 16),
+            name: nft.token_info?.metadata?.name,
+            description: nft.token_info?.metadata?.description,
+            originalContent: { uri: nft.token_info?.metadata?.image }
+          };
+        })
+      : [];
+  }, [rawNfts, collections]);
+  if (!ownedNfts || ownedNfts?.length === 0) {
     return (
       <EmptyState
         message={
@@ -89,9 +92,13 @@ const NftFeed: FC<NftFeedProps> = ({ profile }) => {
     );
   }
 
+  if (nftsError) {
+    return <ErrorMessage title={t`Failed to load NFTs feed`} />;
+  }
+
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      {nfts?.map((nft) => (
+      {ownedNfts?.map((nft) => (
         <div key={`${nft?.chainId}_${nft?.contractAddress}_${nft?.tokenId}`}>
           <SingleNft nft={nft} />
         </div>
@@ -99,5 +106,4 @@ const NftFeed: FC<NftFeedProps> = ({ profile }) => {
     </div>
   );
 };
-
 export default NftFeed;
