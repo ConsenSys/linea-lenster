f28caeb2edbab48b738ea7f470bfe6162465540e
Alain Nicolas <alainncls@users.noreply.github.com>
2023-05-17 15:04:14 +0200

    LENS-68 - Collect - "Set multiple recipients for the collect fee" not available (#33)

--- apps/web/src/components/Settings/Allowance/Allowance.tsx
+++ apps/web/src/components/Settings/Allowance/Allowance.tsx
@@ -1,7 +1,4 @@
-import type {
-  ApprovedAllowanceAmount,
-  ApprovedModuleAllowanceAmountQuery
-} from '@lenster/lens';
+import type { ApprovedAllowanceAmount, ApprovedModuleAllowanceAmountQuery } from '@lenster/lens';
 import { CollectModules } from '@lenster/lens';
 import type { FC } from 'react';
 
@@ -14,13 +11,13 @@ interface AllowanceProps {
 const Allowance: FC<AllowanceProps> = ({ allowance }) => {
   return (
     <div className="space-y-4 p-5">
-      {allowance?.approvedModuleAllowanceAmount?.map(
-        (item: ApprovedAllowanceAmount) =>
-          item?.module === CollectModules.RevertCollectModule ? (
-            ''
-          ) : (
-            <Module key={item?.contractAddress} module={item} />
-          )
+      {allowance?.approvedModuleAllowanceAmount?.map((item: ApprovedAllowanceAmount) =>
+        item?.module === CollectModules.RevertCollectModule ||
+        item?.module === CollectModules.MultirecipientFeeCollectModule ? (
+          ''
+        ) : (
+          <Module key={item?.contractAddress} module={item} />
+        )
       )}
     </div>
   );
