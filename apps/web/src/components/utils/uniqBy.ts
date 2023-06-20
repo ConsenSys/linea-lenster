295e9ebc10327cd38b5c53c3cfc7774bdf7a09f3
Sébastien Van Eyck <sebastien.vaneyck@consensys.net>
2023-06-20 15:04:06 +0200

    fix(LENS-121): remove useless reverse call

--- apps/web/src/components/utils/uniqBy.ts
+++ apps/web/src/components/utils/uniqBy.ts
@@ -1,6 +1,6 @@
 export const uniqBy = (arr: Array<any>, key: string) => {
   return Object.values(
-    [...arr].reverse().reduce((m, i) => {
+    arr.reduce((m, i) => {
       m[key.split('.').reduce((a, p) => a?.[p], i)] = i;
       return m;
     }, {})
